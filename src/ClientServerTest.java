3784a870da5405efd2547661c6b46ce60c7583c8
DinoYoshi <128435588+Dino-Yoshi@users.noreply.github.com>
2026-04-13 15:17:24 -0700

    Added comments

--- src/ClientServerTest.java
+++ src/ClientServerTest.java
@@ -3,7 +3,7 @@ public class ClientServerTest {
 
 	public static void main(String[] args) {
 		
-		
+		// Idea is that the GUI has a ClientNetwork Attribute that when fired auto connects to the server.
 		ClientNetwork c;
 		
 		c = new ClientNetwork("localhost", 7777);
