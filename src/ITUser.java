3e7182672ee5a67961c783b4cc7fc2322cfb7fce
DinoYoshi <128435588+Dino-Yoshi@users.noreply.github.com>
2026-04-13 15:34:39 -0700

    Implemented User.java and ITUser.java

---
+++ src/ITUser.java
@@ -0,0 +1,19 @@
+
+public class ITUser extends User {
+	
+	// attribs
+	private int ITUID;
+	private static int count = 0;
+	
+	// constructor
+	public ITUser(String u, String p) {
+		super(u, p);
+		IT = true;
+	}
+	
+	// getters
+	public int getITUID() {
+		return ITUID;
+	}
+	
+}
---
+++ src/User.java
@@ -0,0 +1,96 @@
+
+public class User {
+	
+	// attribs
+	private int UID;
+	private static int count = 0;
+	private String username;
+	private String password;
+	protected boolean IT;
+	private USERSTATUS status;
+	
+	private enum USERSTATUS {ONLINE, AWAY, DND, OFFLINE, NULL};
+	
+	// constructor
+	
+	User(String u, String p){
+		UID = ++count;
+		username = u;
+		password = p;
+		IT = false;
+		status = USERSTATUS.ONLINE;
+	}
+	
+	// methods
+	
+	public String toString() {
+		return getUID() + "," + getUsername() + "," + getPassword() + "," + getStatus() + "," + isIT();
+	}
+	
+	// getters
+
+	public int getUID() {
+		return UID;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public USERSTATUS getStatus() {
+		return status;
+	}
+	
+	public boolean isIT() {
+		return IT;
+	}
+	
+	// setters
+
+	public void setUID(int uID) {
+		UID = uID;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setStatus(int i) {
+		switch(i) {
+		case(0):
+			status = USERSTATUS.ONLINE;
+		break;
+		
+		case(1):
+			status = USERSTATUS.AWAY;
+		break;
+		
+		case(2):
+			status = USERSTATUS.DND;
+		break;
+			
+		case(3):
+			status = USERSTATUS.OFFLINE;
+		break;
+			
+		default:
+			status = USERSTATUS.NULL;
+		break;
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+}
