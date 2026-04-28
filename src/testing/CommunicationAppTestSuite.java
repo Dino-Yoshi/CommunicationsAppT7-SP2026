591ddef1c13d979cad06d440da97fec8c6d0c909
Dino-Yoshi <dinoyoshi.chau@gmail.com>
2026-04-27 17:35:55 -0700

    Added Request Constructor Test

--- src/testing/CommunicationAppTestSuite.java
+++ src/testing/CommunicationAppTestSuite.java
@@ -10,7 +10,7 @@ import org.junit.platform.suite.api.SelectClasses;
 // Specify Class Selection
 @SelectClasses({UserConstructor.class, UserGetUID.class, UserGetUserName.class, UserGetPassword.class, UserGetStatus.class,
 	UserIsIT.class, UserSetUsername.class, UserSetUID.class, UserSetPassword.class, UserSetStatus.class, ITUserConstructor.class,
-	ITUserGetITUID.class})
+	ITUserGetITUID.class, RequestConstructor.class})
 
 
 
