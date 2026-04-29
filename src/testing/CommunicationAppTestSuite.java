9238da851b689a30b4a134e7de3ca247e6bb300b
Dino-Yoshi <dinoyoshi.chau@gmail.com>
2026-04-29 14:48:04 -0700

    Reformatted Test Suite

--- src/testing/CommunicationAppTestSuite.java
+++ src/testing/CommunicationAppTestSuite.java
@@ -33,6 +33,31 @@ import org.junit.platform.suite.api.SelectClasses;
 	RequestGetSenderID.class, 
 	RequestGetRecipientID.class,
 	
+	ServerNetworkConstructor.class, 
+	ServerNetworkGetNumConnections.class, 
+	ServerNetworkGetPort.class, 
+	ServerNetworkGetStatus.class,
+	
+	ClientNetworkConstructor.class, 
+	ClientNetworkGetServerIP.class, 
+	ClientNetworkGetServerPort.class, 
+	ClientNetworkGetClientSock.class,
+	ClientNetworkGetStatus.class, 
+	ClientNetworkSetIP.class, 
+	ClientNetworkSetPort.class, 
+	ClientNetworkSetClientSock.class, 
+	ClientNetworkSetStatus.class,
+	
+	RequestConstructor.class, 
+	RequestGetUID.class, 
+	RequestGetCreatedDate.class, 
+	RequestGetData.class,
+	RequestGetSenderType.class, 
+	RequestGetRecipientType.class, 
+	RequestGetType.class, 
+	RequestGetSenderID.class, 
+	RequestGetRecipientID.class,
+	
 	AuthRegisterUser.class,
     AuthDuplicateUser.class,
     AuthValidUser.class,
@@ -72,14 +97,7 @@ import org.junit.platform.suite.api.SelectClasses;
     StorageSaveMessage.class,
     StorageLoadChatHistory.class,
 
-@SelectClasses({UserConstructor.class, UserGetUID.class, UserGetUserName.class, UserGetPassword.class, UserGetStatus.class,
-	UserIsIT.class, UserSetUsername.class, UserSetUID.class, UserSetPassword.class, UserSetStatus.class, ITUserConstructor.class,
-	ITUserGetITUID.class, RequestConstructor.class, RequestGetUID.class, RequestGetCreatedDate.class, RequestGetData.class,
-	RequestGetSenderType.class, RequestGetRecipientType.class, RequestGetType.class, RequestGetSenderID.class, RequestGetRecipientID.class,
-	ServerNetworkConstructor.class, ServerNetworkGetNumConnections.class, ServerNetworkGetPort.class, ServerNetworkGetStatus.class,
-	ClientNetworkConstructor.class, ClientNetworkGetServerIP.class, ClientNetworkGetServerPort.class, ClientNetworkGetClientSock.class,
-	ClientNetworkGetStatus.class, ClientNetworkSetIP.class, ClientNetworkSetPort.class, ClientNetworkSetClientSock.class, 
-	ClientNetworkSetStatus.class,
+    
 	})
 
 
